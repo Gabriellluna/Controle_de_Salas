ed05171b86a7449e2424fb0d7ad05b74ba355daa
Gabriellluna <glunamaia@gmail.com>
2024-07-18 22:16:44 -0300

    Segundo commit

--- reserva_app/app.py
+++ reserva_app/app.py
@@ -2,47 +2,80 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-def linha_para_usuarios(linha):
-    dados = linha.strip().split(",")
-    return {
-        'email': dados[0],
-        'senha': dados[1],
-     }
-
-def arquivo():
-     with open("dados/usuarios.csv") as usuarios:
-            for linha in usuarios:
-                lista_usuarios = []
-                usuario = linha_para_usuarios(linha)
-                lista_usuarios.append(usuario)
-                
-
-@app.route("/", methods=["GET", "POST"])
-def login():
+def verifica_usuario():
     if request.method == "POST":
-        with open("usuarios.txt", "w") as usuarios:
+        with open("usuarios.csv") as usuarios:
+                       
+            for linha in usuarios:
+                dados = linha.split(",").strip()
+                print(dados)
+        return redirect(url_for("cadastrar_sala"))           
+    else:
+        return render_template("login.html")
+
+
+               
+def salvar_usuario(email, senha):
+    texto = f"\n{email},{senha}"
+    with open("usuarios.csv", "a") as usuarios:
+        usuarios.write(texto)
+        
+def salvar_sala(tipo, capacidade, descricao):
+    texto = f"\n{tipo},{capacidade},{descricao}"
+    with open("salas.csv", "a") as salas:
+        salas.write(texto)
+ 
+
+@app.route("/", methods=["GET","POST"])
+def cadastro():
+    if request.method == "POST":
+        with open("usuarios.csv", "w") as usuarios:
             
             email = request.form.get('email')
             senha = request.form.get('password')
             print(f"EMAIL: {email}")
             print(f"SENHA: {senha}")
             
-            usuarios.write(arquivo())
+            salvar_usuario(email, senha)
                    
+        return redirect(url_for("cadastrar_sala"))           
+    else:
+        return render_template("cadastro.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+       
+        emaill = request.form.get('EMAIL')
+        senhaa = request.form.get('PASSWORD')
+        print(f"EMAILll: {emaill}")
+        print(f"SENHAaa: {senhaa}")
+                    
         return redirect(url_for("cadastrar_sala"))           
     else:
         return render_template("login.html")
 
-@app.route("/cadastro", methods=["GET","POST"])
-def cadastro():
-    if request.method == "POST":
-        return redirect(url_for("reservas"))
-    else:
-        return render_template("cadastro.html")
+
 
 @app.route("/cadastrar-sala")
 def cadastrar_sala():
-    return render_template("cadastrar-sala.html")
+    if request.method == "POST":
+        with open("usuarios.csv", "w") as usuarios:
+      
+            tipo = request.form.get('tipo')
+            capacidade = request.form.get('capacidade')
+            descricao = request.form.get('descricao')
+
+            print(f"tipo: {tipo}")
+            print(f"capacidade: {capacidade}")
+            print(f"descricao: {descricao}")
+
+            salvar_sala(tipo,capacidade,descricao)
+            
+        return redirect(url_for("reservas"))           
+    else:
+        return render_template("cadastrar-sala.html")
 
 @app.route("/detalhe-reserva")
 def detalhe_reserva():
