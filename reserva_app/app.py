4ca68474f7433475bf88bde1a104be115808a239
Gabriellluna <glunamaia@gmail.com>
2024-07-23 11:28:01 -0300

    Cadastro de usuários e salas

--- reserva_app/app.py
+++ reserva_app/app.py
@@ -2,80 +2,86 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-def verifica_usuario():
-    if request.method == "POST":
-        with open("usuarios.csv") as usuarios:
+# def verifica_usuario():
+#     if request.method == "POST":
+#         with open("usuarios.csv") as usuarios:
                        
-            for linha in usuarios:
-                dados = linha.split(",").strip()
-                print(dados)
-        return redirect(url_for("cadastrar_sala"))           
-    else:
-        return render_template("login.html")
+#             for linha in usuarios:
+#                 dados = linha.split(",").strip()
+#                 print(dados)
+#         return redirect(url_for("cadastrar_sala"))           
+#     else:
+#         return render_template("login.html")
 
 
                
-def salvar_usuario(email, senha):
-    texto = f"\n{email},{senha}"
-    with open("usuarios.csv", "a") as usuarios:
-        usuarios.write(texto)
+
+
+# def verifica_usuario(email, senha):
+#     with open("usuarios.csv") as usuarios:
+#         for linha in usuarios:
+#             print(linha)
+
+def salvar_usuario(nome, sobrenome,email, senha):
+    texto = f"{nome},{sobrenome},{email},{senha}\n"
+    with open("usuarios.csv", "a") as arquivo_usuarios:
+        arquivo_usuarios.write(texto)
+    
         
 def salvar_sala(tipo, capacidade, descricao):
-    texto = f"\n{tipo},{capacidade},{descricao}"
-    with open("salas.csv", "a") as salas:
-        salas.write(texto)
+    texto = f"{tipo},{capacidade},{descricao}\n"
+    with open("salas.csv", "a") as arquivo_salas:
+        arquivo_salas.write(texto)
  
+@app.route("/", methods=["GET"])
+def cadastro_form():
+    return render_template("cadastro.html")
 
-@app.route("/", methods=["GET","POST"])
+
+@app.route("/", methods=["POST"])
 def cadastro():
-    if request.method == "POST":
-        with open("usuarios.csv", "w") as usuarios:
-            
-            email = request.form.get('email')
-            senha = request.form.get('password')
-            print(f"EMAIL: {email}")
-            print(f"SENHA: {senha}")
-            
-            salvar_usuario(email, senha)
-                   
-        return redirect(url_for("cadastrar_sala"))           
-    else:
-        return render_template("cadastro.html")
+    nome = request.form.get('nome-cadastro')
+    sobrenome = request.form.get('sobrenome-cadastro')
+    email = request.form.get('email-cadastro')
+    senha = request.form.get('password-cadastro')
+    print(email)
+    salvar_usuario(nome,sobrenome,email, senha)           
+    return redirect(url_for("cadastrar_sala"))
 
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-       
-        emaill = request.form.get('EMAIL')
-        senhaa = request.form.get('PASSWORD')
-        print(f"EMAILll: {emaill}")
-        print(f"SENHAaa: {senhaa}")
-                    
-        return redirect(url_for("cadastrar_sala"))           
-    else:
-        return render_template("login.html")
-
-
-
-@app.route("/cadastrar-sala")
-def cadastrar_sala():
-    if request.method == "POST":
-        with open("usuarios.csv", "w") as usuarios:
-      
-            tipo = request.form.get('tipo')
-            capacidade = request.form.get('capacidade')
-            descricao = request.form.get('descricao')
-
-            print(f"tipo: {tipo}")
-            print(f"capacidade: {capacidade}")
-            print(f"descricao: {descricao}")
-
-            salvar_sala(tipo,capacidade,descricao)
-            
-        return redirect(url_for("reservas"))           
-    else:
+@app.route("/cadastro-sala", methods=["GET"])
+def cadastrar_sala_form():
         return render_template("cadastrar-sala.html")
+    
+@app.route("/cadastro-sala", methods=["POST"])
+def cadastrar_sala():      
+    tipo = request.form.get('tipo')
+    capacidade = request.form.get('capacidade')
+    descricao = request.form.get('descricao')
+    dado = descricao.replace("\r\n", " ")
+    print(dado)
+    salvar_sala(tipo,capacidade,dado)               
+    return redirect(url_for("reservar_sala"))           
+        
+
+
+# @app.route("/login", methods=["GET", "POST"])
+# def login():
+#         email = request.form.get('email-login')
+#         senha = request.form.get('password-login')
+#         verifica_usuario(email, senha)
+   
+#         return render_template("login.html")
+
+
+
+
+@app.route("/reservar-sala", methods=["GET"])
+def reservar_sala_form():
+    return render_template("reservar-sala.html")
+
+@app.route("/reservar-sala", methods=["POST"])
+def reservar_sala():
+    return render_template("reservar-sala.html")
 
 @app.route("/detalhe-reserva")
 def detalhe_reserva():
@@ -85,11 +91,11 @@ def detalhe_reserva():
 def listar_salas():
     return render_template("listar-salas.html")
 
-@app.route("/reservar-sala")
-def reservar_sala():
-    return render_template("reservar_sala.html")
+
 
 @app.route("/reservas")
 def reservas():
     return render_template("reservas.html")
 
+
+app.run(debug=True)
